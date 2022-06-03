e55b38aba028001e08142f074b4103f5bba1d605
atcolab <s13tc2@protonmail.com>
2022-06-03 17:20:28 -0400

    add exercises 2.11 - 2.14

--- part2/countries/countries-exercise/README.md
+++ part2/countries/countries-exercise/README.md
@@ -2,3 +2,7 @@
 
 [Forms](https://fullstackopen.com/en/part2/forms)
 
+1. Store the initial state of the application in the file db.json, which should be placed in the root of the project.
+2. The API https://restcountries.com provides data for different countries in a machine-readable format, a so-called REST API. Create an application, in which one can look at data of various countries. The application should probably get the data from the endpoint all.
+3. Improve on the application in the previous exercise, such that when the names of multiple countries are shown on the page there is a button next to the name of the country, which when pressed shows the view for that country.
+4. Add to the view showing the data of a single country, the weather report for the capital of that country. There are dozens of providers for weather data. One suggested API is https://openweathermap.org. 
--- part2/countries/countries-exercise/src/App.js
+++ part2/countries/countries-exercise/src/App.js
@@ -21,6 +21,7 @@ const Filter = ({ value, onChange }) => {
   )
 }
 
+
 const Content = ({ country, setCountry }) => {
   if (country.length > 10) {
     return (
@@ -31,8 +32,8 @@ const Content = ({ country, setCountry }) => {
   } else if ((country.length > 2 && country.length < 10) || country.length === 0) {
     return (
       <ul>
-        {country.map((c, i) => 
-          <li key={i}> {c.name.common} <button onClick={() => setCountry([c])}>show</button></li>
+        {country.map((c, i) =>
+          <li key={i}> {c.name.common} <button onClick={() => console.log(setCountry([c]))}>show</button></li>
         )}
       </ul>
     )
@@ -48,17 +49,39 @@ const Content = ({ country, setCountry }) => {
 const Country = ({ country }) =>  {
   const [weather, setWeather] = useState([])
 
+  const country_capital = country.map(c => c.capital)
+
+
+  useEffect(() => {
+    const params = {
+      access_key: process_env.REACT_APP_API_KEY,
+      query: country_capital
+    }
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${country_capital}&appid=${params.access_key}`)
+      .then(response => {
+        const apiResponse = response.data;
+        // console.log(apiResponse)
+        // console.log(`Current temperature in ${country_capital} is ${apiResponse.main.temp}℃`);
+        setWeather([apiResponse])
+      }).catch(error => {
+        console.log(error);
+      })
+
+  })
 
 
   return (
     <div>
-      {/* {country.map(c => console.log(JSON.stringify(c.languages)))} */}
       {country.map(c => <Header text={c.name.common}/>)}
       {country.map(c => <Text text='capital' value={c.capital} />)}
       {country.map(c => <Text text='area' value={c.area} />)}
       {country.map(c => <Header Header text='languages:' />)}
       {country.map(c => <ul>{Object.values(c.languages).map(val => <Lang value={val} />)}</ul>)}
       {country.map(c => <Img src={c.flag}/>)}
+      <h2>Weather in {country_capital}</h2>
+      <p>temperature: {weather.map(w => w.main.temp)}° Celcius</p>
+      <img src={weather.map(w => w.weather[0].icon)} alt="Weather icon"></img>
+      <p>wind: {weather.map(w => w.main.speed)} mph direction {weather.map(w => w.wind.deg)}</p>
     </div>
   )
 }
